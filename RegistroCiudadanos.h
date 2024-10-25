a89dbcd7e7cbeea76fc1bda9e08c8cf841aec1d8
SebastianPortocarre <143566350+SebastianPortocarre@users.noreply.github.com>
2024-10-25 01:18:51 -0500

    Update RegistroCiudadanos.h

--- RegistroCiudadanos.h
+++ RegistroCiudadanos.h
@@ -1,20 +1,15 @@
-// RegistroCiudadanos.h
 #ifndef REGISTROCIUDADANOS_H
 #define REGISTROCIUDADANOS_H
-
 #include <vector>
 #include <cstdint>
 #include <string>
 #include <fstream>
 #include "Ciudadano.h"
 #include "Tablas.h"
-#include "CuckooHashTab.h" // Asegúrate de incluir el encabezado
-
 class RegistroCiudadanos {
 public:
     RegistroCiudadanos();
     ~RegistroCiudadanos();
-
     // Funciones principales
     void generarCiudadanosAleatorios(int cantidad);    // Generar ciudadanos aleatorios
     void insertarCiudadanoManual();                     // Insertar ciudadano manualmente
@@ -23,20 +18,12 @@ public:
     void imprimirTresDniAleatorios();                   // Imprimir tres DNIs aleatorios
     void exportarACSV(const std::string& nombre_archivo); // Exportar datos a CSV
     Tablas tablas;
-
 private:
     std::vector<CiudadanoOptimizado> ciudadanos; // Vector que almacena a todos los ciudadanos
-
-    // Bitmap para rastrear DNIs ya utilizados y evitar duplicados
     std::vector<bool> used_dnis;
 
-    // Rango válido de DNI
     static const uint32_t DNI_MIN = 10000000;
     static const uint32_t DNI_MAX = 99999999;
-
-    // Instancia de CuckooHashTab
-    CuckooHashTab cuckooHashTable;
-
     // Funciones auxiliares para generar datos aleatorios
     uint32_t generarDniAleatorio();
     std::string generarNombreAleatorio();
@@ -44,10 +31,8 @@ private:
     uint32_t generarTelefonoAleatorio();
     std::string generarEmailAleatorio(const std::string& nombre, const std::string& dominio);
     std::string generarUbicacionAleatoria();
-
     // Funciones para cargar y guardar datos
     bool cargarDesdeArchivo(const std::string& nombre_archivo_binario);
     bool guardarEnArchivo(const std::string& nombre_archivo_binario);
 };
-
 #endif // REGISTROCIUDADANOS_H
